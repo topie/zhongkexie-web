572591300b105d5df0d980a7beb6a77390d851e0
chenJz1012 <597160667@qq.com>
2017-12-20 21:17:48 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -18,7 +18,46 @@
         page: function (title) {
             App.content.empty();
             App.title(title);
-            var content = $('<div id="main"><img style="padding: 10px 10px 10px 10px; width: 100%" height="680" src="cdn/img/welcome.jpg"></div>');
+            var content = $('<div class="panel-body" >' +
+                '<div class="row">' +
+                '<div class="col-md-6" >' +
+                '<div class="panel panel-default" >' +
+                '<div class="panel-heading">板块1</div>' +
+                '<div class="panel-body" id="content1"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-6" >' +
+                '<div class="panel panel-default" >' +
+                '<div class="panel-heading">板块2</div>' +
+                '<div class="panel-body" id="content2"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-6" >' +
+                '<div class="panel panel-default" >' +
+                '<div class="panel-heading">板块3</div>' +
+                '<div class="panel-body" id="content3"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-6" >' +
+                '<div class="panel panel-default" >' +
+                '<div class="panel-heading">板块4</div>' +
+                '<div class="panel-body" id="content4"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-6" >' +
+                '<div class="panel panel-default" >' +
+                '<div class="panel-heading">板块5</div>' +
+                '<div class="panel-body" id="content5"></div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="col-md-6" >' +
+                '<div class="panel panel-default" >' +
+                '<div class="panel-heading">板块6</div>' +
+                '<div class="panel-body" id="content6"></div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>');
             App.content.append(content);
             initEvents();
         }
